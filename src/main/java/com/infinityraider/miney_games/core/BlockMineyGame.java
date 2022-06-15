b819d2b91173ab7edefb6f282641426d30cec7e1
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-15 17:38:23 +0100

    Some fixes

--- src/main/java/com/infinityraider/miney_games/core/BlockMineyGame.java
+++ src/main/java/com/infinityraider/miney_games/core/BlockMineyGame.java
@@ -18,36 +18,29 @@ import java.util.List;
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public abstract class BlockMineyGame<T extends TileEntityBase> extends BlockBaseTile<T> {
-    public static final InfProperty<Direction> ORIENTATION = InfProperty.Creators.createHorizontals("orientation", Direction.NORTH);
 
-    private final InfPropertyConfiguration properties;
-
-    private final InfProperty<Integer> size;
-    private final InfProperty<Integer> x;
-    private final InfProperty<Integer> y;
+    private Props props;
 
     public BlockMineyGame(String name, Properties properties) {
         super(name, properties);
-        this.size = InfProperty.Creators.create("size", 1, 1, this.getAllSizes().size());
-        this.x = InfProperty.Creators.create("x", 0, 0, this.getMaxWidth() - 1);
-        this.y = InfProperty.Creators.create("y", 0, 0, this.getMaxDepth() - 1);
-        this.properties = InfPropertyConfiguration.builder()
-                .add(this.size)
-                .add(this.x)
-                .add(this.y)
-                .add(ORIENTATION)
-                .build();
+    }
+
+    private Props getProps() {
+        if(this.props == null) {
+            this.props = new Props(this);
+        }
+        return this.props;
     }
 
     @Override
     protected final InfPropertyConfiguration getPropertyConfiguration() {
-        return this.properties;
+        return this.getProps().getConfiguration();
     }
 
     protected abstract List<MineyGameSize> getAllSizes();
 
     public MineyGameSize getSize(BlockState state) {
-        return this.getAllSizes().get(this.size.fetch(state) - 1);
+        return this.getAllSizes().get(this.getProps().getSizeIndex(state));
     }
 
     public int getWidth(BlockState state) {
@@ -73,19 +66,19 @@ public abstract class BlockMineyGame<T extends TileEntityBase> extends BlockBase
     }
 
     public int getX(BlockState state) {
-        return this.x.fetch(state);
+        return this.props.getX(state);
     }
 
     public int getY(BlockState state) {
-        return this.y.fetch(state);
+        return this.props.getY(state);
     }
 
     public BlockState setOrientation(BlockState state, Direction orientation) {
-        return ORIENTATION.apply(state, orientation);
+        return this.props.setOrientation(state, orientation);
     }
 
     public Direction getOrientation(BlockState state) {
-        return ORIENTATION.fetch(state);
+        return this.props.getOrientation(state);
     }
 
     public abstract VoxelShape getShape(BlockState state);
@@ -130,5 +123,53 @@ public abstract class BlockMineyGame<T extends TileEntityBase> extends BlockBase
     @SuppressWarnings("deprecation")
     public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
         return this.getShape(state, world, pos, context);
+    }
+
+    private static final class Props {
+        public static final InfProperty<Direction> ORIENTATION = InfProperty.Creators.createHorizontals("orientation", Direction.NORTH);
+
+        private final InfPropertyConfiguration properties;
+
+        private final InfProperty<Integer> size;
+        private final InfProperty<Integer> x;
+        private final InfProperty<Integer> y;
+
+        private Props(BlockMineyGame<?> block) {
+            this.size = InfProperty.Creators.create("size", 1, 1, block.getAllSizes().size());
+            this.x = InfProperty.Creators.create("x", 0, 0, block.getMaxWidth() - 1);
+            this.y = InfProperty.Creators.create("y", 0, 0, block.getMaxDepth() - 1);
+            this.properties = InfPropertyConfiguration.builder()
+                    .add(this.size)
+                    .add(this.x)
+                    .add(this.y)
+                    .add(ORIENTATION)
+                    .build();
+
+        }
+
+        public int getSizeIndex(BlockState state) {
+            return this.size.fetch(state) - 1;
+        }
+
+        public final InfPropertyConfiguration getConfiguration() {
+            return this.properties;
+        }
+
+        public final int getX(BlockState state) {
+            return this.x.fetch(state);
+        }
+
+        public final int getY(BlockState state) {
+            return this.y.fetch(state);
+        }
+
+        public final BlockState setOrientation(BlockState state, Direction orientation) {
+            return ORIENTATION.apply(state, orientation);
+        }
+
+        public final Direction getOrientation(BlockState state) {
+            return ORIENTATION.fetch(state);
+        }
+
     }
 }
