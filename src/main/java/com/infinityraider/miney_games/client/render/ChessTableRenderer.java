3bab8a36a7d41962cb68ae0a9ee1182248ff277a
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-21 00:19:49 +0100

    Update chess table square highlighting logic

--- src/main/java/com/infinityraider/miney_games/client/render/ChessTableRenderer.java
+++ src/main/java/com/infinityraider/miney_games/client/render/ChessTableRenderer.java
@@ -4,6 +4,7 @@ import com.infinityraider.infinitylib.reference.Constants;
 import com.infinityraider.infinitylib.render.IRenderUtilities;
 import com.infinityraider.infinitylib.render.tile.ITileRenderer;
 import com.infinityraider.miney_games.MineyGames;
+import com.infinityraider.miney_games.content.chess.ChessGameWrapper;
 import com.infinityraider.miney_games.content.chess.TileChessTable;
 import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -16,55 +17,85 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ChessTableRenderer implements ITileRenderer<TileChessTable>, IRenderUtilities {
+    private static final int ALPHA = 64;
+    private static final float Y = (float) (12*Constants.UNIT + 0.001);
+
     @Override
     public void render(TileChessTable tile, float partialTicks, PoseStack transforms, MultiBufferSource buffer, int light, int overlay) {
         // TODO: render chess pieces
-        this.drawSquareHighlight(tile, transforms, buffer);
+        this.drawSquareHighlights(tile, transforms, buffer);
     }
 
-    protected void drawSquareHighlight(TileChessTable tile, PoseStack transforms, MultiBufferSource buffer) {
+    protected void drawSquareHighlights(TileChessTable tile, PoseStack transforms, MultiBufferSource buffer) {
         Player player = MineyGames.instance.getClientPlayer();
         if(player == null) {
             return;
         }
-        tile.rayTrace(player).ifPresent(hitResult -> {
-            // check if the ray trace was successful
-            if(hitResult.getType() == HitResult.Type.MISS) {
-                return;
+        ChessGameWrapper game = tile.getWrapper();
+        if(!game.isRunning()) {
+            return;
+        }
+        game.asParticipant(player).ifPresent(participant -> {
+            if(participant.hasSelected()) {
+                // highlight selected square blue
+                this.highLightSelectedSquare(tile, participant.getSelectedX(), participant.getSelectedY(), transforms, buffer);
+                // highlight potential moves green
+                participant.getPotentialMovesForSelected().forEach(move -> this.highLightSquare(tile, move.getX(), move.getY(), transforms, buffer, 0, 255, 0));
+            } else {
+                // highlight the square being hovered over
+                tile.rayTrace(player).ifPresent(hitResult -> {
+                    // check if the ray trace was successful
+                    if(hitResult.getType() == HitResult.Type.MISS) {
+                        return;
+                    }
+                    // fetch highlighted square indices
+                    Vec3 hit = tile.offsetAbs(hitResult.getLocation());
+                    int iX = tile.getChessSquareIndexAbsX(hit);
+                    int iZ = tile.getChessSquareIndexAbsY(hit);
+                    this.highLightSquare(tile, iX, iZ, transforms, buffer, 0, 0, 255);
+                });
             }
-            // fetch highlighted square indices
-            Vec3 hit = tile.offsetAbs(hitResult.getLocation());
-            int iX = tile.getChessSquareIndexAbsX(hit);
-            int iZ = tile.getChessSquareIndexAbsY(hit);
-            if(iX < 0 || iZ < 0) {
-                return;
-            }
-            // calculate highlight rendering bounds
-            double x1 = Math.max(tile.getChessSquareMin(iX) - tile.getAbsX(), 0);
-            double x2 = Math.min(tile.getChessSquareMax(iX) - tile.getAbsX(), 1);
-            double z1 = Math.max(tile.getChessSquareMin(iZ) - tile.getAbsY(), 0);
-            double z2 = Math.min(tile.getChessSquareMax(iZ) - tile.getAbsY(), 1);
-            // check if the bounds are within the block's position
-            if(x1 >= 1 || x2 <= 0 || z1 >= 1 || z2 <= 0) {
-                return;
-            }
-            VertexConsumer builder = buffer.getBuffer(HighLightRenderType.INSTANCE);
-            if(!(builder instanceof BufferBuilder)) {
-                return;
-            }
-            this.drawSquare(builder, transforms, x1, z1, x2, z2, 12*Constants.UNIT + 0.001, 0, 0, 255, 64);
+            // if the king is in check, highlight the king in red
+            participant.getPiecesOnBoard("king").forEach(square -> {
+                if(participant.isAttacked(square)) {
+                    this.highLightSquare(tile, square.getX(), square.getY(), transforms, buffer, 255, 0, 0);
+                }
+            });
         });
     }
 
-    protected void drawSquare(VertexConsumer builder, PoseStack transforms, double x1, double z1, double x2, double z2, double y, int r, int g, int b, int a) {
-        this.drawVertex(builder, transforms, x1, y, z1, r, g, b, a);
-        this.drawVertex(builder, transforms, x1, y, z2, r, g, b, a);
-        this.drawVertex(builder, transforms, x2, y, z2, r, g, b, a);
-        this.drawVertex(builder, transforms, x2, y, z1, r, g, b, a);
+    protected void highLightSelectedSquare(TileChessTable tile, int xRel, int yRel, PoseStack transforms, MultiBufferSource buffer) {
+        int iX = tile.getOrientation().xRelToAbs(xRel, yRel, tile.getSize());
+        int iY = tile.getOrientation().yRelToAbs(xRel, yRel, tile.getSize());
+        this.highLightSquare(tile, iX, iY, transforms, buffer, 0, 0, 255);
     }
 
-    protected void drawVertex(VertexConsumer builder, PoseStack transforms, double x, double y, double z, int r, int g, int b, int a) {
-        builder.vertex(transforms.last().pose(), (float) x, (float) y, (float) z).color(r, g, b, a).endVertex();
+    protected void highLightSquare(TileChessTable tile, int iX, int iZ, PoseStack transforms, MultiBufferSource buffer, int r, int g, int b) {
+        if(iX < 0 || iZ < 0) {
+            return;
+        }
+        // calculate highlight rendering bounds
+        double x1 = Math.max(tile.getChessSquareMin(iX) - tile.getAbsX(), 0);
+        double x2 = Math.min(tile.getChessSquareMax(iX) - tile.getAbsX(), 1);
+        double z1 = Math.max(tile.getChessSquareMin(iZ) - tile.getAbsY(), 0);
+        double z2 = Math.min(tile.getChessSquareMax(iZ) - tile.getAbsY(), 1);
+        // check if the bounds are within the block's position
+        if(x1 >= 1 || x2 <= 0 || z1 >= 1 || z2 <= 0) {
+            return;
+        }
+        VertexConsumer builder = buffer.getBuffer(HighLightRenderType.INSTANCE);
+        this.drawSquare(builder, transforms, x1, z1, x2, z2, r, g, b);
+    }
+
+    protected void drawSquare(VertexConsumer builder, PoseStack transforms, double x1, double z1, double x2, double z2, int r, int g, int b) {
+        this.drawVertex(builder, transforms, x1, z1, r, g, b);
+        this.drawVertex(builder, transforms, x1, z2, r, g, b);
+        this.drawVertex(builder, transforms, x2, z2, r, g, b);
+        this.drawVertex(builder, transforms, x2, z1, r, g, b);
+    }
+
+    protected void drawVertex(VertexConsumer builder, PoseStack transforms, double x, double z, int r, int g, int b) {
+        builder.vertex(transforms.last().pose(), (float) x, Y, (float) z).color(r, g, b, ALPHA).endVertex();
     }
 
     public static class HighLightRenderType extends RenderType {
